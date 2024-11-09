f3ca82f701410af0ce3bc2d62171f05631c2c555
joviedoj <oviedoj@commitmarket.com>
2024-11-09 13:37:30 -0700

    Changing button test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <Button text="click me"/>
+        <Button text="random button"/>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
