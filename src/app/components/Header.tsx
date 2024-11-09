7a3d919ca38cb0e244b42c46b41ee3fdbe259451
joviedoj <oviedoj@commitmarket.com>
2024-11-09 13:18:31 -0700

    Fixing build issues

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,10 @@
 'use client'
  
-export default function Header(props: any) {
+export default function Header() {
     return (
-        <div className="w-100 h-[50px] bg-blue-500"> Logo</div>
+        <div className="w-100 h-[50px] bg-blue-500 flex items-center flex-row justify-between"> 
+            <div className="m-2 w-32">Menu</div>
+            <div className="m-2">Logo</div>
+        </div>
     )
 }
